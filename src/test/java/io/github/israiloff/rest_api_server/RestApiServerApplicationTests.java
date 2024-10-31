993f659e474096fb1479f2b026647c7bfc7dab75
Israiloff <stardustindark@gmail.com>
2024-10-31 18:55:13 +0500

    initial commit;

---
+++ src/test/java/io/github/israiloff/rest_api_server/RestApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.israiloff.rest_api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
