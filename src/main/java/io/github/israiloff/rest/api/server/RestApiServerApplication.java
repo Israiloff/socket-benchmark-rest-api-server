67fdbf2772497e82320f1986993fe81fdf336008
Israiloff <stardustindark@gmail.com>
2024-11-01 18:05:21 +0500

    added: package scanning;

--- src/main/java/io/github/israiloff/rest/api/server/RestApiServerApplication.java
+++ src/main/java/io/github/israiloff/rest/api/server/RestApiServerApplication.java
@@ -3,7 +3,7 @@ package io.github.israiloff.rest.api.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "io.github.israiloff")
 public class RestApiServerApplication {
 
 	public static void main(String[] args) {
